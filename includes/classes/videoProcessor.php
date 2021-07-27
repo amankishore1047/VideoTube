c252cbcfb01d2eb72021fb200eaf086dcc3c67a2
Aman Kishore <amankishorepaul@gmail.com>
2021-07-28 05:01:07 +0530

    videoInsertData function added videos are now stored in the database table videos.

--- includes/classes/videoProcessor.php
+++ includes/classes/videoProcessor.php
@@ -24,7 +24,13 @@ class VideoProcessor {
         if(!$isValidData)   return false;
 
         if(move_uploaded_file($videoData["tmp_name"], $tempFilePath)) {
-            echo "File moved successfully";
+            $finalPath = $targetDir . uniqid() . ".mp4";
+
+            if(!$this->insertVideoData($videoUploadData, $finalPath)){
+                echo "Insert query failed.";
+                return false;
+
+            }
         }
     }
 
@@ -57,6 +63,20 @@ class VideoProcessor {
 
     private function hasError($data) {
         return $data["error"] != 0;
+    }
+
+    private function insertVideoData($uploadData, $filePath){
+            $query = $this->con->prepare("INSERT INTO videos(title, uploadedBy, description, privacy, category, filePath)
+                                        VALUES(:title, :uploadedBy, :description, :privacy, :category, :filePath)");
+                                    
+            $query->bindParam(":title", $uploadData->title);
+            $query->bindParam(":uploadedBy", $uploadData->uploadedBy);
+            $query->bindParam(":description", $uploadData->description);
+            $query->bindParam(":privacy", $uploadData->privacy);
+            $query->bindParam(":category", $uploadData->category);
+            $query->bindParam(":filePath", $filePath);
+
+            return $query->execute();
     }
 }
 ?>
